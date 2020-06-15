88a15cfeb65fd7d0b9e29b7f00a7a64d614a166b
Ushnesha Daripa <sweetydaripa@gmail.com>
2020-06-15 13:52:33 +0530

    Reverse a String

---
+++ Reverse a String.cpp
@@ -0,0 +1,20 @@
+string Solution::solve(string A) {
+    //put all words in a vectors
+    vector<string> words;
+    string s="";
+    for(int i=0;i<A.length();i++){
+        if(A[i]==' ') continue;
+        s+=A[i];
+        if((i==A.length()-1) || (A[i+1]==' ')) {
+            words.push_back(s);
+            s="";
+        }
+    }
+    s="";
+    //from the vector iterate reversely and add it to string along with a space
+    for(int i=words.size()-1; i>=0;i--){
+        s+=words[i];
+        if(i!=0) s+=' ';
+    }
+    return s;
+}
